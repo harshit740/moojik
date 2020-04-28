24dd5f706dc0dc245f254ce89f4af7a852ed5f20
harshit740 <harsh.008.com@gmail.com>
2020-04-28 20:57:39 +0530

    Upgrade gradel and added Androidx artifcacts

--- lib/src/utils/songLyrics.dart
+++ lib/src/utils/songLyrics.dart
@@ -2,17 +2,28 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
 import 'package:html/dom.dart' as dom;
 
-getSongLyrics(query) async {
-
-  print("query = $query lyrics");
-  var url = 'https://www.google.com/search?q=$query+ lyrics';
+getSongLyrics(String name) async {
+  name = name.toLowerCase();
+  name = name.replaceAll(RegExp("\\(([Oo]]fficial)?\\s([Mm]usic)?\\s([Vv]ideo)?\\s\\)"), "");
+  name = name.replaceAll(RegExp("\\(\\[?[Ll]yrics?\\)]?\\s?([Vv]ideo)?\\)?"), "");
+  name = name.replaceAll(RegExp("\\(?[aA]udio\\)?\\s"), "");
+  name = name.replaceAll(RegExp("\\[Lyrics?]"), "");
+  name = name.replaceAll(RegExp("\\(Official\\sMusic\\sVideo\\)"), "");
+  name = name.replaceAll(RegExp("\\[HD\\s&\\sHQ]"), "");
+  name = name.split("(")[0];
+  print("query = $name lyrics");
+  var url = 'https://www.google.com/search?q=$name+ lyrics';
   Map<String, String> headers = {
     "user-agent":
     "Mozilla/5.0 (Linux; U; Tizen 2.0; en-us) AppleWebKit/537.1 (KHTML, like Gecko) Mobile TizenBrowser/2.0"
   };
   var response = await http.get(url,headers: headers);
   dom.Document document =  parser.parse(response.body);
+  try{
   var lyrics = document.querySelectorAll('div.hwc');
-
   return lyrics.first.children.first.children.first.children.first.innerHtml;
+  }
+  catch(e){
+    return "Lyrics NotFound try typing the song name mannualy";
+  }
 }
